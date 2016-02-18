2f8db287c88eb397b0d92f0c5154edf30634fe2a
Yanis <yanis.urbis@gmail.com>
2016-02-18 19:45:01 +0300

    Template Level Subscription

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,4 +1,11 @@
-Meteor.subscribe('recipes')
+// Meteor.subscribe('recipes')
+
+Template.Recipes.onCreated(function() {
+  var self = this;
+  self.autorun(function() {
+    self.subscribe('recipes')
+  })
+})
 
 // console.log(Meteor.settings.public.ga.account)
 

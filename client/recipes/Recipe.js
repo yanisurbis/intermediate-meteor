272c036d37460c0f0ab2159dd90a3a72744705ed
Yanis <yanis.urbis@gmail.com>
2016-02-20 23:52:53 +0300

    Deleting Recipes and Session Helpers.

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -1,6 +1,12 @@
 Template.Recipe.events({
   'click .toggle-menu': function() {
     Meteor.call('toggleMenuItem', this._id, this.inMenu)
+  },
+  'click .fa-trash': function() {
+    Meteor.call('deleteRecipe', this._id)
+  },
+  'click .fa-pencil': function() {
+    Session.set('editMode', !Session.get('editMode'))
   }
 })
 
